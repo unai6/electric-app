a798c7b01be2696d9b98c17fc13b24adcba58300
Unai Gonzalez <unaigoe91@gmail.com>
2022-06-05 10:18:58 +0200

    Clean code

--- src/components/RealTimeInsularPrice.vue
+++ src/components/RealTimeInsularPrice.vue
@@ -47,24 +47,24 @@ onMounted(async () => {
 
 // TODO: If this function is used anywhere else, place it into a config file.
 function hexToRGBA(h, opacity) {
-  let r = 0, g = 0, b = 0;
+  let r = 0, g = 0, b = 0
 
   // 3 digits
   if (h.length == 4) {
-    r = "0x" + h[1] + h[1];
-    g = "0x" + h[2] + h[2];
-    b = "0x" + h[3] + h[3];
+    r = "0x" + h[1] + h[1]
+    g = "0x" + h[2] + h[2]
+    b = "0x" + h[3] + h[3]
 
   // 6 digits
   } else if (h.length == 7) {
-    r = "0x" + h[1] + h[2];
-    g = "0x" + h[3] + h[4];
-    b = "0x" + h[5] + h[6];
+    r = "0x" + h[1] + h[2]
+    g = "0x" + h[3] + h[4]
+    b = "0x" + h[5] + h[6]
   }
-  return `rgba(${+r},${+g},${+b},${opacity})`;
+  return `rgba(${+r},${+g},${+b},${opacity})`
 }
 
-function getGradient (context) {1
+function getGradient (context) {
   if (context) {
     const chart = context.chart
     const { ctx } = chart
