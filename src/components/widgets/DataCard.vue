32ab7ae8a7e4f198b885a0e6b529bff943b4e1be
Unai Gonzalez <unaigoe91@gmail.com>
2022-06-19 16:57:16 +0200

    Add timeTrunc prices at dashboard

--- src/components/widgets/DataCard.vue
+++ src/components/widgets/DataCard.vue
@@ -19,15 +19,33 @@ const props = defineProps({
       {{ props.title }}
     </h3>
     <slot />
+    <div v-if="props.isLoading" class="data-card__loader">
+      <div class="loading loading--big" />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .data-card {
+  position: relative;
   display: flex;
   flex-direction: column;
   background: shade($black-color, lighter-2);
   padding: $spacer;
   border-radius: $small-radius;
+
+  &__loader {
+    position: absolute;
+    margin: -$spacer;
+    padding-top: $spacer*2;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: top;
+    background-color: rgba($broken-white-color, 0.6);
+    border-radius: $small-radius;
+    z-index: 1;
+  }
 }
 </style>
